784fccd1efdedc4bbf439d4dc79d9acb23c3409e
Michelle CAO <jiahu0724417@gmail.com>
2020-08-11 11:48:41 +0800

    add new basic line

--- read.py
+++ read.py
@@ -12,3 +12,11 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 print('留言的平均長度為', sum_len / len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言')
+print(new[0])
+print(new[1])
